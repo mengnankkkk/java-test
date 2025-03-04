2bbc582c301ffa767503c5f91288cb2c782d3e99
mengnankkkk <mengnankkzhou@gmail.com>
2025-03-04 16:06:27 +0800

    杯子里的水面积最大

--- Solution.java
+++ Solution.java
@@ -64,3 +64,20 @@ class Solution4 {
         }
     }
 }
+class Solution5{
+    public int maxArea(int[] height){
+        int res = 0;
+        int i = 0;
+        int j = height.length-1;
+        while (i<j){
+            int area = (j-i)*Math.min(height[i],height[j]);
+            res = Math.max(res,area);
+            if (height[i]<height[j]){
+                i++;
+            }else {
+                j--;
+            }
+        }
+        return res;
+    }
+}
