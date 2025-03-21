cfd389c6ed93885551780ad22e1f25cba610c1fd
mengnankkkk <mengnankkzhou@gmail.com>
2025-03-21 17:38:31 +0800

    最近公共点

--- Solution.java
+++ Solution.java
@@ -604,3 +604,13 @@ class Solution33{
         cnt.merge(s,-1,Integer::sum);//归零
     }
 }
+class Solution34{
+    public TreeNode lowestCommonAncestor(TreeNode root,TreeNode p,TreeNode q){
+        if (root==null||root==p||root==q) return root;
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if (left==null) return right;
+        if (right==null) return left;
+        return root;
+    }
+}
