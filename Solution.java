4002e6a8596d16048371c83c9addda295543322a
mengnankkkk <mengnankkzhou@gmail.com>
2025-04-06 15:31:05 +0800

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1551,3 +1551,30 @@ class Solution69{
         nums[index2] = temp;
     }
 }
+class Solution70{
+    private void swap(int[] nums,int i,int j){
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+    private void reverse(int[] nums,int left,int right){
+        while (left<right){
+            swap(nums,left++,right--);
+        }
+    }
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n-2;
+        while (i>=0&&nums[i]>=nums[i+1]){
+            i--;
+        }
+        if (i>=0){
+            int j = n-1;
+            while (nums[j]<=nums[i]){
+                j--;
+            }
+            swap(nums,i,j);
+        }
+        reverse(nums,i+1,n-1);
+    }
+}
