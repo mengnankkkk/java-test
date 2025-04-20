2c669f932d95b74f4ae4a19ed38f099363eba481
mengnankkkk <mengnankkzhou@gmail.com>
2025-04-20 22:39:53 +0800

    Create leetcode.java

---
+++ leetcode.java
@@ -0,0 +1,33 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class leetcode {
+}
+class Solution2300{
+    public int[] successfulPairs(int[] spells, int[] potions, long success){
+        Arrays.sort(potions);
+        for (int i=0;i<spells.length;i++){
+            long target = (success-1)/spells[i];
+            if (target<potions[potions.length-1]){
+                spells[i] = potions.length-upperBound(potions,(int)target);
+            }else {
+                spells[i]=0;
+
+            }
+        }
+        return spells;
+    }
+    private int upperBound(int[] nums,int target){
+        int left = -1,right = nums.length;
+        while (left+1<right){
+            int mid = (left+right)>>>1;
+            if (nums[mid]>target){
+                right = mid;
+            }
+            else {
+                left = mid;
+            }
+        }
+        return right;
+    }
+}
