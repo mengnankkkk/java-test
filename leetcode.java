4d5cae611bd6ed23336df6cfd89e2c9c97f6545d
mengnankkkk <mengnankkzhou@gmail.com>
2025-04-21 15:54:24 +0800

    Update leetcode.java

--- leetcode.java
+++ leetcode.java
@@ -1,5 +1,6 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Map;
 
 public class leetcode {
 }
@@ -29,5 +30,57 @@ class Solution2300{
             }
         }
         return right;
+    }
+}
+class Solution2145{
+    public int numberOfArrays(int[] differences,int lower,int upper){
+        long s = 0,minS = 0,maxS = 0;
+        for (int d:differences){
+            s+=d;
+            minS = Math.min(minS,s);
+            maxS = Math.max(maxS,s);
+        }
+        return (int) Math.max(upper-lower-maxS+minS+1,0);
+    }
+}
+class Solution1385{
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d){
+        Arrays.sort(arr2);
+        int ans = 0;
+        for (int x:arr1){
+            int i  = Arrays.binarySearch(arr2,x-d);
+            if (i<0){
+                i=~i;
+            }
+            if (i==arr2.length||arr2[i]>x+d){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
+class Solution2389{
+    private int upperBound(int[] nums,int target){
+        int left = -1,right = nums.length;
+        while (left+1<right){
+            int mid = left+(right-left)/2;
+            if (nums[mid]>target){
+                right = mid;
+            }
+            else {
+                left = mid;
+            }
+        }
+        return right;
+    }
+    public int[] answerQueries(int[] nums, int[] queries){
+        Arrays.sort(nums);
+        for (int i =1;i<nums.length;i++){
+            nums[i]+=nums[i-1];
+        }
+        for (int i =0;i<queries.length;i++){
+            queries[i] = upperBound(nums,queries[i]);
+        }
+        return queries;
     }
 }
