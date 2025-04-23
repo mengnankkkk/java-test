5c04d0efd8830710cd989a0510cbcd0f4a9494aa
mengnankkkk <mengnankkzhou@gmail.com>
2025-04-23 16:05:11 +0800

    Update leetcode.java

--- leetcode.java
+++ leetcode.java
@@ -84,3 +84,71 @@ class Solution2389{
         return queries;
     }
 }
+class Solution1170{
+    public int[] numSmallerByFrequency(String[] queries, String[] words){
+        int n = words.length;
+        int[] nums = new int[n];
+        for (int i =0;i<n;++i){
+            nums[i] = f(words[i]);
+        }
+        Arrays.sort(nums);
+        int m  = queries.length;
+        int[] ans = new int[m];
+        for (int i =0;i<m;++i){
+            int x = f(queries[i]);
+            int l=0,r= n;
+            while (l<r){
+                int mid = (l+r)>>1;
+                if(nums[mid]>x){
+                    r = mid;
+                }
+                else {
+                    l = mid+1;
+                }
+            }
+            ans[i]=n-l;
+        }
+        return ans;
+
+    }
+    private int f(String s){
+        int[] cnt = new int[26];
+        for (int i =0;i<s.length();++i){
+            ++cnt[s.charAt(i)-'a'];
+        }
+        for (int x:cnt){
+            if (x>0){
+                return x;
+            }
+        }
+        return 0;
+    }
+}
+class Solution1399{
+    private int calcDigitSum(int num){
+        int ds = 0;
+        while (num>0){
+            ds +=num%10;
+            num/=10;
+        }
+        return ds;
+    }
+    public int countLargestGroup(int n){
+        int m  = String.valueOf(n).length();
+        int[] cnt = new int[m*9+1];
+        int maxCnt = 0;
+        int ans = 0;
+        for (int i =1;i<=n;i++){
+            int ds = calcDigitSum(i);
+            cnt[ds]++;
+            if (cnt[ds]>maxCnt){
+                maxCnt = cnt[ds];
+                ans = 1;
+            }else if (cnt[ds]==maxCnt){
+                ans++;
+            }
+        }
+        return ans;
+
+    }
+}
